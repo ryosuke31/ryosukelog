d79eb1b4694696e2ef8868bdf4c16d8cad043a85
ryosuke_kamiya <Skyriver753@gmail.com>
2022-05-31 23:30:06 +0900

    [add]microCMSからブログを取得

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,35 @@
-import type { NextPage } from "next";
+import { MicroCMSListResponse } from "microcms-js-sdk";
+import type { GetStaticProps, NextPage } from "next";
+import { client } from "src/libs/client";
 
-const Home: NextPage = () => {
-  return <div className="text-blue-500">Hello!</div>;
+export type Blog = {
+  title: string;
+  body: string;
+};
+
+type Props = MicroCMSListResponse<Blog>;
+
+const Home: NextPage<Props> = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      <ul>
+        {props.contents.map((content) => {
+          return <li key={content.id}>{content.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const data = await client.getList({
+    endpoint: "blog",
+  });
+  return {
+    props: data,
+  };
 };
 
 export default Home;
